02845af5e6526621dfad3c3739389688c684b026
Bobby Chen <rbchen16@gmail.com>
2021-01-16 20:43:02 -0500

    Bugfix: clicking START while the timer is running increments the timer

--- public/javascript/utilities.js
+++ public/javascript/utilities.js
@@ -3,32 +3,41 @@ console.log("Loaded JavaScript Utilities");
 var seconds = 0;
 var timer;
 
+var isTimerRunning = false;
+
 
 function start(element, clr) {
-	// Make the first second quicker so it doesn't feel as bad to click the button
-	seconds++;
-	displayMinutes = Math.floor(seconds / 60);
-	displaySeconds = seconds % 60;
-  	updateTimerView(displayMinutes, displaySeconds);
+	if (!isTimerRunning) {
+		isTimerRunning = true;
 
-	// Update the count down every 1 second
-	timer = setInterval(function() {
+		// Make the first second quicker so it doesn't feel as bad to click the button
 		seconds++;
-
 		displayMinutes = Math.floor(seconds / 60);
 		displaySeconds = seconds % 60;
-
 	  	updateTimerView(displayMinutes, displaySeconds);
 
-		// If the count down is finished, write some text
-		if (displayMinutes >= 1) {
-			clearInterval(x);
-			document.getElementById("main-timer").innerHTML = "EXPIRED";
-		}
-	}, 1000);
+		// Update the count down every 1 second
+		timer = setInterval(function() {
+			seconds++;
+
+			displayMinutes = Math.floor(seconds / 60);
+			displaySeconds = seconds % 60;
+
+		  	updateTimerView(displayMinutes, displaySeconds);
+
+			// If the count down is finished, write some text
+			if (displayMinutes >= 1) {
+				clearInterval(x);
+				document.getElementById("main-timer").innerHTML = "EXPIRED";
+			}
+		}, 1000);
+	} else {
+		pause(element, clr);
+	}
 }
 
 function pause(element, clr) {
+	isTimerRunning = false;
 	clearInterval(timer);
 }
 
